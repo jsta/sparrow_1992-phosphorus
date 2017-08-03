500ed823cde7b690ad9d5efc1dfdede5495aefc0
jsta <jstachelek@utexas.edu>
2017-08-03 12:50:47 -0400

    enable dbf loading

--- R/load.R
+++ R/load.R
@@ -1,21 +1,31 @@
 #' erf_load
 #' @description Load files from local file system
 #' @param version_id character database version string
-#' @param format character choice of rds
+#' @param format character choice of rds or dbf
 #' @param fpath file.path optionally specify custom location of rds file
 #' @export
-#' @importFrom rappdirs user_data_dir
+#' @importFrom foreign read.dbf
 #' @examples \dontrun{
 #' erf  <- erf_load("1")
 #' }
 erf_load <- function(version_id, format = "rds", fpath = NA){
 
-  if(!is.na(fpath)){
-      readRDS(fpath)
+  if(format == "rds"){
+    if(!is.na(fpath)){
+        readRDS(fpath)
+    }else{
+        rds_path <- paste0(cache_path(), "erf_", version_id, ".rds")
+        stop_if_not_exists(rds_path)
+        readRDS(rds_path)
+    }
   }else{
-      rds_path <- paste0(cache_path(), "erf_", version_id, ".rds")
-      stop_if_not_exists(rds_path)
-      readRDS(rds_path)
+    if(!is.na(fpath)){
+      foreign::read.dbf(fpath)
+    }else{
+      dbf_path <- file.path(cache_path(), version_id, "ARC.dbf")
+      stop_if_not_exists(dbf_path)
+      foreign::read.dbf(dbf_path)
+    }
   }
 }
 
