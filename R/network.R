8f1cfc6913d5ba13ba3e7c67e9f4dc124507cae4
jsta <jstachelek@utexas.edu>
2017-08-08 11:41:52 -0400

    use polygon overlay not point buffer to select lines

--- R/network.R
+++ R/network.R
@@ -8,17 +8,33 @@ get_downstream_ids <- function(id_lake, erf){
   erf[erf$FNODE_ %in% tnodes,]$MRB_ID
 }
 
-assign_reach_position <- function(id_lake, id_upstream, id_downstream,
-                                  polygon, erf){
-  erf_sub <- erf[erf$MRB_ID %in% c(id_lake, id_upstream, id_downstream),]
+assign_reach_position <- function(id_lake, dbf, polygon, erf){
+
+  dbf <- dbf[!duplicated(dbf$MRB_ID),]
+
+  fnodes <- dbf[dbf$MRB_ID %in% id_lake$intersects, ]
+
+  # fnodes_erf <- erf[erf$MRB_ID %in% fnodes$MRB_ID,]
+  # mapview::mapview(fnodes_erf)
+
+  fnodes_upstream   <- fnodes[!(fnodes$FNODE_ %in% fnodes$TNODE_),]
+  fnodes_downstream <- fnodes[!(fnodes$TNODE_ %in% fnodes$FNODE_),]
+  fnodes_focal <- fnodes[!(fnodes$MRB_ID %in% fnodes_downstream$MRB_ID) &
+                           !(fnodes$MRB_ID %in% fnodes_upstream$MRB_ID), ]
+
+  erf_sub <- erf[erf$MRB_ID %in% fnodes$MRB_ID,]
   erf_sub <- sf::st_transform(erf_sub, sf::st_crs(polygon))
 
   erf_sub$position <- "focal"
-  erf_sub[erf_sub$MRB_ID %in% id_downstream, "position"] <- "downstream"
-  erf_sub[erf_sub$MRB_ID %in% id_upstream, "position"]   <- "upstream"
+  erf_sub[erf_sub$MRB_ID %in%
+            fnodes_downstream$MRB_ID, "position"] <- "downstream"
+  erf_sub[erf_sub$MRB_ID %in%
+            fnodes_upstream$MRB_ID, "position"]   <- "upstream"
   erf_sub$position <- factor(erf_sub$position,
                              levels = c("upstream", "focal", "downstream"))
-  erf_sub
+  list(erf_sub = erf_sub,
+       up_ids = fnodes_upstream$MRB_ID,
+       down_ids = fnodes_downstream$MRB_ID)
 }
 
 
@@ -34,9 +50,9 @@ assign_reach_position <- function(id_lake, id_upstream, id_downstream,
 #'
 network <- function(lon, lat, lines, dbf, polygon){
   id_lake  <- get_id(lon, lat, lines, polygon)
-  up_ids   <- get_upstream_ids(id_lake, dbf)
-  down_ids <- get_downstream_ids(id_lake, dbf)
-  reaches  <- assign_reach_position(id_lake, up_ids, down_ids, polygon, lines)
+  reaches  <- assign_reach_position(id_lake, dbf, polygon, lines)
 
-  list(upstream_ids = up_ids, downstream_ids = down_ids, reaches = reaches)
+  list(upstream_ids = reaches$up_ids,
+       downstream_ids = reaches$down_ids,
+       reaches = reaches$erf_sub)
 }
--- R/query.R
+++ R/query.R
@@ -11,15 +11,24 @@
 #'
 #' @export
 #'
+#'
 get_id <- function(lon, lat, lines, poly){
 
   pnt <- sf::st_sfc(sf::st_point(c(lon, lat)))
   st_crs(pnt) <- 4326
-  erf_sub <- nhdR::select_point_overlay(pnt, lines, buffer_dist = 0.3)
 
-  id_lake <- erf_sub[unlist(lapply(sf::st_intersects(erf_sub, poly),
-                                   length)) > 0,]
-  id_lake$MRB_ID
+  poly_sub <- nhdR::select_point_overlay(pnt, poly, buffer_dist = 0.2)
+
+  within_lines  <- lines[sapply(st_within(lines, poly),
+                                 function(x){length(x) > 0}),]
+  # crosses_lines <- lines[sapply(st_crosses(lines, poly),
+  #                               function(x){length(x) > 0}),]
+  # mapview::mapview(list(crosses_lines), zcol = list("MRB_ID"))
+
+  intersects_lines <- lines[sapply(st_intersects(lines, poly),
+                                   function(x){length(x) > 0}),]
+
+  list(intersects = intersects_lines$MRB_ID, within = within_lines$MRB_ID)
 }
 
 #' @importFrom sf st_buffer st_as_sf st_intersection
